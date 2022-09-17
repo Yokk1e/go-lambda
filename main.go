9aee93737aca386c7365814841c7c16dd1e1b7f3
Yok-1337 <satthabut.o@gmail.com>
2022-09-18 01:27:35 +0700

    feat: update

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -11,13 +13,41 @@ func main() {
 	lambda.Start(handler)
 }
 
-func handler(reuqest events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Println("Hello world")
+	var person Person
+	err := json.Unmarshal([]byte(request.Body), &person)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	msg := fmt.Sprintf("Hello %v %v", *person.FirstName, *person.LastName)
+
+	reponseBody := ResponseBody{
+		Message: &msg,
+	}
+
+	jbytes, err := json.Marshal(reponseBody)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
 
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
+		Body:       string(jbytes),
 	}
 
 	return response, nil
 
+}
+
+type Person struct {
+	FirstName *string `json:"firstName"`
+	LastName  *string `json:"lastName"`
+}
+
+type ResponseBody struct {
+	Message *string `json:"message"`
 }
